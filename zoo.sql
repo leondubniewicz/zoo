e0c8c31227a28a5aed1950f0a7c7cd7089a79478
leondubniewicz <kokslole@gmail.com>
2023-09-05 14:47:22 +0200

    zoo

---
+++ zoo.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `pracownicy` DROP FOREIGN KEY `pracownicy_fk0`;
+
+ALTER TABLE `zwierzenta` DROP FOREIGN KEY `zwierzenta_fk0`;
+
+ALTER TABLE `opiekuny` DROP FOREIGN KEY `opiekuny_fk0`;
+
